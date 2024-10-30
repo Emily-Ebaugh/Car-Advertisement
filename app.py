e9b88783d7fce998caa0a03a0aa0ded1dc653d2c
Emily-Ebaugh <sfcc.emily.ebaugh@gmail.com>
2024-10-29 23:41:21 -0700

    Load the dataset vehicles_us.csv

--- app.py
+++ app.py
@@ -7,3 +7,10 @@ st.write("Step 1: create '.streamlit/config.toml'")
 st.write("Step 2: submit to github")
 st.write("Step 3: connect to Render.com and make sure your application will be run on it")
 st.write("Step 4: Finish the coding job")
+
+
+# Load the dataset
+data = pd.read_csv('vehicles_us.csv')
+
+# Display the first few rows of the dataset
+print(data.head())
