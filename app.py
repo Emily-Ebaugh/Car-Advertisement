81ae43513e24c09b633531377e505fa48fae2159
Emily-Ebaugh <sfcc.emily.ebaugh@gmail.com>
2024-10-29 23:57:20 -0700

    test .head of dataset on app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import pandas as pd
 
 st.write("Hello world")
 st.button("Click")
@@ -8,6 +9,7 @@ st.write("Step 2: submit to github")
 st.write("Step 3: connect to Render.com and make sure your application will be run on it")
 st.write("Step 4: Finish the coding job")
 
+import pandas as pd
 
 # Load the dataset
 data = pd.read_csv('vehicles_us.csv')
