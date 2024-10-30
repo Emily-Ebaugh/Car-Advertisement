75c575b08c24d671afe2356066c38ec55434fc11
Emily-Ebaugh <sfcc.emily.ebaugh@gmail.com>
2024-10-29 23:17:58 -0700

    create .streamlit/config.toml and make serverAddress 0.0.0.0

--- app.py
+++ app.py
@@ -1,3 +1,9 @@
 import streamlit as st
 
 st.write("Hello world")
+st.button("Click")
+
+st.write("Step 1: create '.streamlit/config.toml'")
+st.write("Step 2: submit to github")
+st.write("Step 3: connect to Render.com and make sure your application will be run on it")
+st.write("Step 4: Finish the coding job")
