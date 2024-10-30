abec13c16c9b3fc68424831bc7faccd3e917f8cc
Emily-Ebaugh <sfcc.emily.ebaugh@gmail.com>
2024-10-30 12:00:10 -0700

    deleted directions

--- app.py
+++ app.py
@@ -6,10 +6,6 @@ import altair as alt
 st.write("Hello world")
 st.button("Click")
 
-st.write("Step 1: create '.streamlit/config.toml'")
-st.write("Step 2: submit to github")
-st.write("Step 3: connect to Render.com and make sure your application will be run on it")
-st.write("Step 4: Finish the coding job")
 
 
 # Specify the path to the CSV file
